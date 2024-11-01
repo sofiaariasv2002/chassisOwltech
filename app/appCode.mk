3b2fcd0e4a33d6aec8b90e385627952d5ca83f6a
JorgePerC <jorgeperez_1@live.com>
2024-11-01 04:36:27 +0000

    set build settings for Freertos

--- app/appCode.mk
+++ app/appCode.mk
@@ -1,8 +1,9 @@
-# This file contains compilation instructions dependent on the development board.
+# This file contains compilation instructions of the application and FreeRTOS.
 # THIS IS NOT A STANDALONE MAKE FILE. 
 # ==========================================
 # Possible improvements:
-# * Don't compile everything, only the enabled modules at stm32f1xx_hal_conf.h
+# * Make for an easy way to change the heap type 
+# * Set a variable for when the processor has a Memory Protected Unit
 # ==========================================
 # Made by Jorge Pérez 4 Owltech <3
 
@@ -24,24 +25,21 @@
 # * Depending on the port, it may also be necessary for the same directories to be in the assembler's include path.
 
 # Add FreeRTOS includes 
-ifdef USE_FREERTOS
+ifeq ($(USE_FREERTOS), 1)
 C_INCLUDES += \
 	-I$(FirmwarePath)/Middlewares/Third_Party/FreeRTOS/Source/include \
 	-I$(FirmwarePath)/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS \
-	-I$(FirmwarePath)/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM3 \
+	-I$(FirmwarePath)/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/$(ARCH_ARM) \
 	-Iapp/inc
 endif
 
 # Add FreeRTOS sources 
-ifdef USE_FREERTOS
-
-# TODO: know what to use in memmang, and in cm3 with or without mpu 
-C_SOURCES += \
-	C_SOURCES += $(shell find $(FirmwarePath)/Middlewares/Third_Party/FreeRTOS/Source/ -maxdepth 1 -name *.c) \
-	C_SOURCES += $(shell find $(FirmwarePath)/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/ -name *.c) \
-	C_SOURCES += $(shell find $(FirmwarePath)/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM3/ -name *.c) \
-	C_SOURCES += $(shell find $(FirmwarePath)/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/ -name *.c)
+ifeq ($(USE_FREERTOS), 1)
+	C_SOURCES += $(shell find $(FirmwarePath)/Middlewares/Third_Party/FreeRTOS/Source/ -maxdepth 1 -name *.c)
+	C_SOURCES += $(shell find $(FirmwarePath)/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/ -name *.c)
+	C_SOURCES += $(shell find $(FirmwarePath)/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/$(ARCH_ARM)/ -name *.c)
+	C_SOURCES += $(shell find $(FirmwarePath)/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_1.c)
+	C_SOURCES += $(shell find app/src/*.c)
 # C_SOURCES += $(shell find $(FirmwarePath)/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM3_MPU/ -name *.c)
-
 endif
 
